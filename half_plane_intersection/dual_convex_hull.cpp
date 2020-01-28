a096fe3a50a6c0313782b60fa045c2b5de318d56
Nikolai Zakharevich <n.zakharevich@corp.vk.com>
2020-01-28 16:58:24 +0300

    optimized

--- half_plane_intersection/dual_convex_hull.cpp
+++ half_plane_intersection/dual_convex_hull.cpp
@@ -12,26 +12,28 @@ ll product(const plane &x, const plane &y);
 
 struct point {
     double x, y;
+
+    point(double x, double y) : x(x), y(y) {}
 };
 
 struct plane {
     ll a, b, c;
-    int num;
+    int idx;
 
     plane() = default;
 
-    plane(ll a, ll b, ll c, int num) : a(a), b(b), c(c), num(num) {}
-
-    bool operator<(const plane &rhs) const {
-        return atan2((double) -a, (double) b) < atan2((double) -rhs.a, (double) rhs.b);
-    }
+    plane(ll a, ll b, ll c, int num) : a(a), b(b), c(c), idx(num) {}
 
     bool operator==(const plane &rhs) const {
-        return det(plane(a, b, c, num), rhs) == 0 && product(plane(a, b, c, num), rhs) > 0;
+        return !det(plane(a, b, c, idx), rhs) && product(plane(a, b, c, idx), rhs) > 0;
     }
 
     bool operator!=(const plane &rhs) const {
         return !(rhs == *this);
+    }
+
+    bool operator<(const plane &rhs) const {
+        return atan2((double) -a, b) < atan2((double) -rhs.a, rhs.b);
     }
 };
 
@@ -39,31 +41,28 @@ ll det(const plane &x, const plane &y) {
     return x.a * y.b - y.a * x.b;
 }
 
-point intersectionPoint(const plane &x, const plane &y) {
-    double d = det(x, y);
-    return {-(x.c * y.b - y.c * x.b) / d, -(x.a * y.c - y.a * x.c) / d};
-}
-
 ll product(const plane &x, const plane &y) {
     return x.a * y.a + x.b * y.b;
 }
 
-ll turn_det(const plane &x, const plane &y, const plane &z) {
+auto turn_det(const plane &x, const plane &y, const plane &z) {
     return x.a * y.b * z.c - x.c * y.b * z.a + x.c * y.a * z.b - x.b * y.a * z.c + x.b * y.c * z.a - x.a * y.c * z.b;
+}
+
+bool left_turn(const plane &x, const plane &y, const plane &z) {
+    return turn_det(x, y, z) > 0;
 }
 
 auto min(const plane &x, const plane &y) {
     if (x < y) return x;
-    if ((y.b && x.c * y.b == y.c * x.b) || (!y.b && (x.c * y.a == x.a * y.c))) {
-        return x.num < y.num ? x : y;
-    }
-    auto p = x.b != 0 ?  point{.0, (double) -x.c / x.b} : point{(double) -x.c / x.a, .0};
+    if ((y.b && x.c * y.b == y.c * x.b) || (!y.b && (x.c * y.a == x.a * y.c))) return x.idx < y.idx ? x : y;
+    auto p = x.b != 0 ? point{.0, (double) -x.c / x.b} : point{(double) -x.c / x.a, .0};
     return y.a * p.x + y.b * p.y + y.c > 0 ? x : y;
 }
 
 bool isZero(const plane &x, const plane &y) {
     if (det(x, y) || product(x, y) >= 0) return false;
-    auto p = y.a != 0 ?  point{(double) -y.c / y.a, .0} : point{.0, (double) -y.c / y.b};
+    auto p = y.a != 0 ? point{(double) -y.c / y.a, .0} : point{.0, (double) -y.c / y.b};
     return (x.a * p.x + x.b * p.y + x.c) <= 0;
 }
 
@@ -73,6 +72,11 @@ bool isINF(const plane &x, const plane &y) {
     return (x.a * p.x + x.b * p.y + x.c) > 0;
 }
 
+
+auto inc(int i, int mod) {
+    return (i + 1) % mod;
+}
+
 double square(const vector<plane> &intersected) {
     auto n = intersected.size();
     if (n == 0) return .0;
@@ -80,9 +84,11 @@ double square(const vector<plane> &intersected) {
 
     vector<point> edges;
     for (int idx = 0; idx < n; idx++) {
-        int nextIdx = (idx + 1) % n;
-        if (isZero(intersected[idx], intersected[nextIdx])) return .0;
-        edges.push_back(intersectionPoint(intersected[idx], intersected[nextIdx]));
+        auto curr = intersected[idx];
+        auto next = intersected[inc(idx, n)];
+        if (isZero(curr, next)) return .0;
+        double d = det(curr, next);
+        edges.emplace_back(-(curr.c * next.b - next.c * curr.b) / d, -(curr.a * next.c - next.a * curr.c) / d);
     }
 
     double res = .0;
@@ -94,86 +100,96 @@ double square(const vector<plane> &intersected) {
     return fabs(res / 2);
 }
 
+void exitInf() {
+    cout << -1;
+    exit(0);
+}
 
-vector<plane> intersected;
+void exitZero() {
+    cout << 0;
+    exit(0);
+}
 
-int buildIntersected(const vector<plane> &unique, int maxNum) {
-    vector<plane> hull{unique.front(), unique[1]};
-    vector<char> used(maxNum);
+int findStart(const vector<plane> &hull) {
+    unordered_set<int> used;
+    for (auto &p: hull) {
+        if (used.find(p.idx) != used.end()) {
+            return p.idx;
+        }
+        used.insert(p.idx);
+    }
+    return -1;
+}
+
+auto buildIntersected(const vector<plane> &unique) {
+    vector<plane> intersected, hull{unique.front(), unique[1]};
 
     for (int i = 2; i < unique.size(); i++) {
-        while (hull.size() >= 2 && turn_det(unique[i], hull[hull.size() - 2], hull[hull.size() - 1]) <= 0) {
+        while (hull.size() >= 2 && !left_turn(unique[i], hull[hull.size() - 2], hull.back())) {
             hull.pop_back();
         }
         hull.push_back(unique[i]);
     }
-    int stack = -1;
-    for (auto &p : hull) {
-        if (used[p.num]) {
-            stack = p.num;
-            break;
-        }
-        used[p.num] = 1;
-    }
 
-    int n = hull.size();
+    auto n = hull.size();
 
+    plane prev = hull.back(), curr;
     for (int i = 0; i < n; i++) {
-        auto b = hull[(i + 1) % n];
-        if (isINF(hull[i], b)) return -1;
-        if (isZero(hull[i], b)) return 0;
-    }
-    if (stack == -1) return 0;
-
-    for (int i = 0; i < n; i++) {
-        if (stack != hull[i].num) continue;
-        intersected.push_back(hull[i]);
-        i = (i + 1) % n;
-        while (stack != hull[i].num) {
-            intersected.push_back(hull[i]);
-            i = (i + 1) % n;
-        }
-        break;
+        curr = hull[i];
+        if (isZero(prev, curr)) exitZero();
+        if (isINF(prev, curr)) exitInf();
+        prev = curr;
     }
 
-    return 1;
+    int start = findStart(hull);
+    if (start == -1) exitZero();
+
+    int idx = -1;
+    while (start != hull[++idx].idx) {}
+
+    intersected.push_back(hull[idx]);
+    idx = inc(idx, n);
+    while (hull[idx].idx != start) {
+        intersected.push_back(hull[idx]);
+        idx = inc(idx, n);
+    }
+
+    return intersected;
 }
 
-int checkIntersection(vector<plane> &planes) {
-    int maxNum = planes.size();
+auto checkAndBuildIntersection(vector<plane> &planes) {
     sort(planes.begin(), planes.end(), [](const plane &x, const plane &y) { return x != y ? x < y : x.c < y.c; });
 
     vector<plane> unique;
-    plane nextUnique = planes.front(), prev, curr = planes.front();
+    plane nextUnique = planes.front(), prevPlane, currPlane = planes.front();
     for (int i = 1; i <= planes.size(); i++) {
-        prev = curr;
-        if (i == planes.size()) {
-            if (unique.empty()) return -1;
-            curr = planes.front();
-        } else {
-            curr = planes[i];
-        }
+        prevPlane = currPlane;
+        bool last = i == planes.size();
+        if (last && unique.empty()) exitInf();
+        currPlane = !last ? planes[i] : planes.front();
 
-        if (curr == prev) {
-            nextUnique = min(nextUnique, curr);
+        if (currPlane == prevPlane) {
+            nextUnique = min(nextUnique, currPlane);
         } else {
             unique.push_back(nextUnique);
-            nextUnique = curr;
+            nextUnique = currPlane;
         }
     }
     unique.front() = nextUnique;
 
     auto n = unique.size();
-    if (n <= 1) return -1;
-    for (int i = 0; i < n; i++) {
-        if (det(unique[i], unique[(i + 1) % n]) < 0) return -1;
-        auto next = unique[(i + 1) % n];
-        if (isZero(unique[i], next) && !det(unique[i], next) && (product(unique[i], next) < 0)) return 0;
+    if (n <= 1) exitInf();
+    for (int idx = 0; idx < n; idx++) {
+        auto curr = unique[idx];
+        auto next = unique[inc(idx, n)];
+        if (det(curr, next) < 0) exitInf();
+        if (isZero(curr, next) && !det(curr, next) && (product(curr, next) < 0)) exitZero();
     }
+
     for (int i = 0; i < n; i++) {
-        unique.emplace_back(unique[i].a, unique[i].b, unique[i].c, unique[i].num);
+        unique.emplace_back(unique[i].a, unique[i].b, unique[i].c, unique[i].idx);
     }
-    return buildIntersected(unique, maxNum);
+    return buildIntersected(unique);
 }
 
 int main() {
@@ -189,16 +205,8 @@ int main() {
         cin >> a >> b >> c;
         planes.emplace_back(a, b, c, i);
     }
-    auto res = checkIntersection(planes);
-    if (res == -1 || res == 0) {
-        cout << res;
-        return 0;
-    }
-    auto sq = square(intersected);
-    if (sq < 0) {
-        cout << -1;
-        return 0;
-    }
+    auto sq = square(checkAndBuildIntersection(planes));
+    if (sq < 0) exitInf();
     cout << setprecision(20) << fixed << sq;
     return 0;
 
